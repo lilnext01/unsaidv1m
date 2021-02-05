1dcce266a0d64436f6aa5e876b71f126929909d9
lilnext01 <74841546+lilnext01@users.noreply.github.com>
2021-02-04 23:55:29 -0300

    Add files via upload

---
+++ Unsaid-main/src/help1.js
@@ -0,0 +1,45 @@
+const help1 = (prefix) => {
+
+	return `
+╭──────────────╮
+    *𝐂𝐎𝐌𝐀𝐍𝐃𝐎𝐒 𝐃𝐎 𝐔𝐍𝐒𝐀𝐈𝐃*
+╰──────────────╯
+
+➸ *${prefix}marcar*➸
+➸ *${prefix}marcar2*
+➸ *${prefix}marcar3*
+➸ *${prefix}loli*
+➸ *${prefix}loli1*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia* _<aguarde um pouco>_
+➸ *${prefix}mia1* _<aguarde um pouco>_
+➸ *${prefix}mia2* _<aguarde um pouco>_ 
+➸ *${prefix}belle* _<aguarde um  tente>_
+➸ *${prefix}belle1*_<aguarde um pouco>_
+➸ *${prefix}belle2* _<aguarde um pouco>_
+➸ *${prefix}belle3* _<aguarde um pouco>_
+➸ *${prefix}akeno* 
+➸ *${prefix}meme* _<off>_
+➸ *${prefix}lofi* 
+➸ *${prefix}malkova*
+➸ *${prefix}canal*
+➸ *${prefix}nsfwloli*
+➸ *${prefix}reislin*
+➸ *${prefix}limpar*
+➸ *${prefix}marcar*
+➸ *${prefix}teste*
+➸ *${prefix}text3d* <texto>
+
+════════════════════
+*UNSAID-V1* 🤗
+*Digite ${prefix}dono para mais info*
+════════════════════`
+
+}
+exports.help1 = help1
+
---
+++ Unsaid-main/src/unsaid.js
@@ -0,0 +1,40 @@
+const unsaid = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *𝐂𝐎𝐌𝐀𝐍𝐃𝐎𝐒 𝐃𝐎 𝐋𝐈𝐋𝐍𝐄𝐗𝐓*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  *𝐂𝐎𝐌𝐀𝐍𝐃𝐎𝐒 𝐃𝐎 𝐋𝐈𝐋𝐍𝐄𝐗𝐓*
+  DUVIDAS? 👇
+  WA.me/555197195835
+╚════════════════════`
+}
+
+exports.darkmenu = unsaid
+
+
+
+
+
+
+
+
